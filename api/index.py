9ed1bc08cfade635c5027cc0cdf5718bbfae6844
VIKAS <97907750+vikas70607@users.noreply.github.com>
2025-05-17 23:31:53 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from db_handler import upsert_entry, get_entry
+from message_handler import send_message, send_session_message
+from openai_handler import create_thread
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/input', methods=['POST'])
+def receive_json():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "No JSON received or invalid format"}), 400
 
-@app.route('/about')
-def about():
-    return 'About'
+    phone_id = data.get('waId')
+    name = data.get('senderName')
+    text = data.get('text')
+
+    if not phone_id or not name or not text:
+        return jsonify({"error": "Missing required fields"}), 400
+
+    print("Received JSON:", phone_id, name, text)
+
+    existing_entry = get_entry(phone_id)
+
+    if existing_entry:
+        thread_id = existing_entry[0]['threadId']
+        message = send_message(text, thread_id)
+        send_session_message(message,phone_id)
+        print('Entry exists, message sent to existing thread')
+        return jsonify({"message": "Entry updated and message sent"}), 200
+    else:
+        thread_id = create_thread(name,text)
+        upsert_entry(phone_id, name, thread_id)
+        message = send_message(text, thread_id)
+        send_session_message(message,phone_id)
+        print('New entry created and message sent')
+        return jsonify({"message": "New entry created and message sent"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
